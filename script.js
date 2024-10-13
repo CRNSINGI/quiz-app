48a3e6bbd30f76565bce48c34b14dc3204851172
Cesar <cesarnsingi@hotmail.com>
2024-10-13 05:10:20 +0400

    define answer opt

--- script.js
+++ script.js
@@ -12,12 +12,12 @@ const questions = [
 
     {
 
-        question: "Which is largest animal in the world?",
+        question: "Which is smallest animal in the world?",
         answers: [
-            {text: "Shark", correct: false},
-            {text: "Blue Whale", correct: true},
-            {text: "Elephant", correct: false},
-            {text: "Giraffe", correct: false},
+            {text: "Myxozoa", correct: true},
+            {text: "Fish", correct: false},
+            {text: "Dog", correct: false},
+            {text: "Cat", correct: false},
 
         ]
 
@@ -25,24 +25,24 @@ const questions = [
 
 
     {
-        question: "Which is largest animal in the world?",
+        question: "Which is largest desert in the world?",
         answers: [
-            {text: "Shark", correct: false},
-            {text: "Blue Whale", correct: true},
-            {text: "Elephant", correct: false},
-            {text: "Giraffe", correct: false},
+            {text: "Kalahari", correct: false},
+            {text: "Gobi", correct: true},
+            {text: "Sahara", correct: false},
+            {text: "Antarctica", correct: true},
 
         ]
     },
 
     {
 
-        question: "Which is largest animal in the world?",
+        question: "Which is the smallest continent in the world?",
         answers: [
-            {text: "Shark", correct: false},
-            {text: "Blue Whale", correct: true},
-            {text: "Elephant", correct: false},
-            {text: "Giraffe", correct: false},
+            {text: "Asia", correct: false},
+            {text: "Australia", correct: true},
+            {text: "Arctic", correct: false},
+            {text: "Africa", correct: false},
 
         ]
 
