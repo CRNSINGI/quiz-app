1d7a5eea7d21fc0628223f6f05f4c3f0f8eb57fc
Cesar <cesarnsingi@hotmail.com>
2024-10-13 04:58:19 +0400

    add more questions opt

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const questions = [
     {
-        question: "Which is largest animal in the world",
+        question: "Which is largest animal in the world?",
         answers: [
             {text: "Shark", correct: false},
             {text: "Blue Whale", correct: true},
@@ -12,6 +12,39 @@ const questions = [
 
     {
 
-        
+        question: "Which is largest animal in the world?",
+        answers: [
+            {text: "Shark", correct: false},
+            {text: "Blue Whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+
+        ]
+
+    },
+
+
+    {
+        question: "Which is largest animal in the world?",
+        answers: [
+            {text: "Shark", correct: false},
+            {text: "Blue Whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+
+        ]
+    },
+
+    {
+
+        question: "Which is largest animal in the world?",
+        answers: [
+            {text: "Shark", correct: false},
+            {text: "Blue Whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+
+        ]
+
     }
 ]
