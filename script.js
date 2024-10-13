d2ff8c033c9e033c8718b4afde1ed47c80aca9d8
Cesar <cesarnsingi@hotmail.com>
2024-10-13 06:37:05 +0400

    final js fix problem

--- script.js
+++ script.js
@@ -113,6 +113,8 @@ function selectAnswer(e){
 function showScore(){
     resetState();
     questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
 }
 
 function handleNextButton(){
