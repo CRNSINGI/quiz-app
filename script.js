e8c6c9ed51729011cd40063d820551e2299509dd
Cesar <cesarnsingi@hotmail.com>
2024-10-13 04:54:29 +0400

    add questions js

---
+++ script.js
@@ -0,0 +1,17 @@
+const questions = [
+    {
+        question: "Which is largest animal in the world",
+        answers: [
+            {text: "Shark", correct: false},
+            {text: "Blue Whale", correct: true},
+            {text: "Elephant", correct: false},
+            {text: "Giraffe", correct: false},
+
+        ]
+    },
+
+    {
+
+        
+    }
+]
